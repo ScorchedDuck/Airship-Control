0dcc9124320e4e255eea77bfc7f2ea0d703d1ef6
ScorchedDuck <hughhodkin@gmail.com>
2026-07-28 09:39:18 +0100

    4

--- startup.lua
+++ startup.lua
@@ -1,1 +1,7 @@
-print("hi")
+local VERSIONS_PATH = "https://raw.githubusercontent.com/ScorchedDuck/Airship-Control/main/versions.json"
+
+local sensor = peripheral.find("optical_sensor")
+
+local idBlock = sensor.getBlock()
+
+print(idBlock)
