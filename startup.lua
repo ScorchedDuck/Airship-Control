4dbc26ee509e6f4205c5084cff5fcd760513a1e6
ScorchedDuck <hughhodkin@gmail.com>
2026-07-28 09:51:23 +0100

    6

--- startup.lua
+++ startup.lua
@@ -3,5 +3,33 @@ local VERSIONS_PATH = "https://raw.githubusercontent.com/ScorchedDuck/Airship-Co
 local sensor = peripheral.find("optical_sensor")
 
 local idBlock = sensor.getBlock()
+local role
 
-print(idBlock)
+local function getVersions()
+    local response = http.get(VERSIONS_PATH)
+    if not response then
+        error("Couldn't download versions.json")
+    end
+
+    local data = textutils.unserializeJSON(response.readAll())
+    response.close()
+
+    return data
+end 
+
+local versions = getVersions()
+
+for name, data in ipairs(versions) do
+    if name ~= "startup" then
+        if data.idBlock == idBlock then
+            role = data
+            break
+        end
+    end
+end
+
+if role then
+    print("Got role: " .. role)
+else 
+    error("No role found for block: " .. idBlock)
+end
