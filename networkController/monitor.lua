4aa08d61f07c8dc65a35aa39613db14c86151287
ScorchedDuck <hughhodkin@gmail.com>
2026-08-25 11:18:32 +0100

    46

--- networkController/monitor.lua
+++ networkController/monitor.lua
@@ -40,7 +40,7 @@ return {
 
         local data = self.network.get(self.requestId)
 
-        if #data > 0 and collected == false then
+        if data and #data > 0 and collected == false then
             self.collected = true
 
             self.current = {}
