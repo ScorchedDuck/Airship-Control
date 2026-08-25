7eba162adcecaa7b976f3a1a0a64f096828fa1b1
ScorchedDuck <hughhodkin@gmail.com>
2026-08-25 18:50:02 +0100

    52

--- monitor/run.lua
+++ monitor/run.lua
@@ -363,7 +363,7 @@ local function maintenanceLoop()
         removeOffline()
         if nextPing <= os.clock() then
             modem.transmit(101, 0, {command = "ping", body = {name = "monitor"}})
-            nextPing = os.clock + 10
+            nextPing = os.clock() + 10
         end
         sleep(1)
     end
